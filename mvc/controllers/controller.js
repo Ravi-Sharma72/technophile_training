fccc51a672b34655aab45d8a8011cade6012ef41
Ravi-Sharma72 <ravi107sharma@gmail.com>
2026-07-25 14:30:07 +0530

    use trycatch

--- mvc/controllers/controller.js
+++ mvc/controllers/controller.js
@@ -1,33 +1,53 @@
 import { userModel } from "../model/model.js";
 
-
-export const addController = async(req, res) => {
-     const { name, email, passward } = req.body;
-  const data = new userModel({ name, email, passward });
-  const result = await data.save();
-  res.send(result);
+export const addController = async (req, res) => {
+  try {
+    const { name, email, passward } = req.body;
+    const data = new userModel({ name, email, passward });
+    const result = await data.save();
+    res.send(result);
+  } catch (error) {
+    console.log(error);
+    res.send("internal server error");
+  }
 };
 
 export const readController = async (req, res) => {
-  const data = await userModel.find();
-  res.send(data);
-}
-export const delController =async (req, res) => {
-  const { _id } = req.params;
-  const result = await userModel.deleteOne({ _id });
-  res.send(result);
-}
+  try {
+    const data = await userModel.find();
+    res.send(data);
+  } catch (error) {
+    console.log(error);
+    res.send("internal server error");
+  }
+};
+export const delController = async (req, res) => {
+  try {
+    const { _id } = req.params;
+    const result = await userModel.deleteOne({ _id });
+    res.send(result);
+  } catch (error) {
+    console.log(error);
+    res.send("internal server error");
+  }
+};
 export const editController = async (req, res) => {
-  const { _id } = req.params;
-  const { name, email, passward } = req.body;
-  const result = await userModel.updateOne(
-    { _id },
-    { $set: { name, email, passward } },
-  );
-  res.send(result);
-}
+  try {
+    const { _id } = req.params;
+    const { name, email, passward } = req.body;
+    const result = await userModel.updateOne(
+      { _id },
+      { $set: { name, email, passward } },
+    );
+    res.send(result);
+  } catch (error) {
+    console.log(error);
+    res.send("internal server error");
+  }
+};
 
 
 
+//now we'll use res.json and send in object
 
- 
+
